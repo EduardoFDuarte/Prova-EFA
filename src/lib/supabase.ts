1e963b6aad8e2f63821326f666ff18a3d8054cc6
EduardoFDuarte <eduardo.duarte@login.works>
2026-07-28 17:05:51 +0100

    Fix Supabase key env var

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,9 +2,13 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
 const supabaseKey = (
-  import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY ||
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 ) as string
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error('Supabase env vars not set! Define VITE_SUPABASE_URL e VITE_SUPABASE_PUBLISHABLE_KEY')
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const supabase = createClient<any>(
